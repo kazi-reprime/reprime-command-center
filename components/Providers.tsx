87c13e83d7ebadf1c4d2ef12abb9d8820b02589a
kazi-reprime <kazi@reprime.com>
2026-06-30 00:08:47 +0530

    fix: Providers syntax error

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -15,6 +15,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
           },
         },
       })
+  )
   return (
     <QueryClientProvider client={client}>
       <ToastProvider>
